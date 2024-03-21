ff0027dc0d5783308c248e14b36a8b7568f9d87b
ROUSE-prog <stevenr2049@gmail.com>
2024-03-21 17:48:36 -0400

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Code to display initial book list
+    // Event listener for form submission
+    // Function to add a new book
+    // Event listener for remove button clicks
+    // Function to remove a book
+  });
+  
